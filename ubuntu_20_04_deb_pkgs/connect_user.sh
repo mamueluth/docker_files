25d0e7067fb8117ea671779c730d0dd57190fa29
Manuel <mamueluth@gmail.com>
2021-12-13 20:53:49 +0100

    add connect user and added real workspace to share

---
+++ ubuntu_20_04_deb_pkgs/connect_user.sh
@@ -0,0 +1,1 @@
+docker exec -u $USER <docker_container> /bin/bash
--- ubuntu_20_04_deb_pkgs/create.sh
+++ ubuntu_20_04_deb_pkgs/create.sh
@@ -1,3 +1,3 @@
 xhost +local:docker
-docker run --net=host --rm -e DISPLAY --tmpfs /tmp -v /tmp/.X11-unix/:/tmp/.X11-unix:rw -v /<path>/<workspace_name>:/$HOME/<workspace_name:rw -it ros2_deb_pkg_user_x11 /bin/bash
+docker run --net=host -e DISPLAY --tmpfs /tmp -v /tmp/.X11-unix/:/tmp/.X11-unix:rw -v $HOME/Workspaces/stogl_robotics/test_ws_shared_1/:$HOME/test_ws:rw --name ros2_deb_pkg_user_x11-instance -it ros2_deb_pkg_user_x11 /bin/bash
 
